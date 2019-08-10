5b764b043e550cf7ab48152f9fb3c49bb8d77f66
anthony vega <anthony.vega@accenture.com>
2019-08-09 23:26:23 -0600

    adding styles

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import './style/global.scss';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <h1>title</h1>
         <p>
           Team Delta
         </p>
--- src/index.js
+++ src/index.js
@@ -5,10 +5,9 @@ import App from './App';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
-/**
- * Example Session Fetching
- * Remember to have the aid cookie for local development!
- */
+//setting aid cookie, for local development
+document.cookie = 'aid=wWtmY7oLilRQxeMaLbCcaJK3jsT3BpId0.2p8eux10uj2;';
+
 fetch('/api/user/session').then(response => {
   if (response.status === 200) {
     response.json().then(session => console.log(session))
